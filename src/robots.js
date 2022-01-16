7088f0de317229607f16fb9d56d57a223d239f90
Ronald van der Rest <ronaldvanderrest@live.nl>
2022-01-16 17:34:42 +0100

    fix vulnerabiltiy

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@April.com',
+	},
+	{
+		id: 2,
+		name: 'Blag Graham',
+		username: 'blag',
+		email: 'blag@April.com',
+	},
+	{
+		id: 3,
+		name: 'Cnne Graham',
+		username: 'Cdan',
+		email: 'Cna@April.com',
+	},
+	{
+		id: 4,
+		name: 'D Graham',
+		username: 'D',
+		email: 'D@April.com',
+	},
+];
